d132a56f1fd4ffdde4117e3c8a77d21b48a56c75
chardub <pkmninfinitefusion@gmail.com>
2025-05-12 11:32:25 -0400

    hoenn pokemon centers

--- 053_PIF_Hoenn/PokemartMapTransfers.rb
+++ 053_PIF_Hoenn/PokemartMapTransfers.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
+
+CITIES_MAP_IDS = {
+  :LITTLEROOT => 9,
+  :OLDALE => 8,
+  :PETALBURG =>7,
+  :RUSTBORO => 0,
+  :DEWFORD => 0,
+  :SLATEPORT => 0,
+  :MAUVILLE => 0,
+  :VERDANTURF => 0,
+  :FALLARBOR => 0,
+  :LAVARIDGE => 0,
+  :FORTREE => 0,
+  :LILYCOVE => 0,
+  :MOSSDEEP => 0,
+  :SOOTOPOLIS => 0,
+  :PACIFIDLOG => 0,
+  :EVERGRANDE => 0,
+  :BATTLEFRONTIER => 0,
+
+}
+
+
 # Necessary dor setting the various events within the pokemart map, uses the numbers as wondertrade
 def get_city_numerical_id_hoenn(city_sym)
   current_city_numerical = {
@@ -43,32 +66,34 @@ end
 #   }
 # end
 
+
+HOENN_POKEMART_ENTRANCES = {
+  :LITTLEROOT => [1, 0, 0],
+  :OLDALE => [1, 0, 0],
+  :VERMILLION => [1, 0, 0],
+  :PETALBURG => [7, 32, 19],
+  :RUSTBORO => [1, 0, 0],
+  :DEWFORD => [1, 0, 0],
+  :SLATEPORT => [1, 0, 0],
+  :MAUVILLE => [1, 0, 0],
+  :VERDANTURF => [1, 0, 0],
+  :FALLARBOR => [1, 0, 0],
+  :LAVARIDGE => [1, 0, 0],
+  :FORTREE => [1, 0, 0],
+  :LILYCOVE => [1, 0, 0],
+  :MOSSDEEP => [1, 0, 0],
+  :SOOTOPOLIS => [1, 0, 0],
+  :PACIFIDLOG => [1, 0, 0],
+  :EVERGRANDE => [1, 0, 0],
+}
 def exit_pokemart_hoenn()
-  pokemart_entrances = {
-    :LITTLEROOT => [1, 0, 0],
-    :OLDALE => [1, 0, 0],
-    :VERMILLION => [1, 0, 0],
-    :PETALBURG => [7, 32, 19],
-    :RUSTBORO => [1, 0, 0],
-    :DEWFORD => [1, 0, 0],
-    :SLATEPORT => [1, 0, 0],
-    :MAUVILLE => [1, 0, 0],
-    :VERDANTURF => [1, 0, 0],
-    :FALLARBOR => [1, 0, 0],
-    :LAVARIDGE => [1, 0, 0],
-    :FORTREE => [1, 0, 0],
-    :LILYCOVE => [1, 0, 0],
-    :MOSSDEEP => [1, 0, 0],
-    :SOOTOPOLIS => [1, 0, 0],
-    :PACIFIDLOG => [1, 0, 0],
-    :EVERGRANDE => [1, 0, 0],
-  }
+
   current_city = pbGet(VAR_CURRENT_MART)
   current_city = :PETALBURG if !current_city.is_a?(Symbol)
 
-  entrance_map = pokemart_entrances[current_city][0]
-  entrance_x = pokemart_entrances[current_city][1]
-  entrance_y = pokemart_entrances[current_city][2]
+  entrance_map = HOENN_POKEMART_ENTRANCES[current_city][0]
+  entrance_x = HOENN_POKEMART_ENTRANCES[current_city][1]
+  entrance_y = HOENN_POKEMART_ENTRANCES[current_city][2]
 
   pbSet(VAR_CURRENT_CITY_NUMERICAL_ID, 0)
   pbSet(VAR_CURRENT_MART, 0)
@@ -85,4 +110,30 @@ end
 def get_mart_exclusive_items_hoenn(city)
   items_list = []
   return items_list
+end
+
+def exit_pokemon_center()
+  if $PokemonGlobal.pokecenterMapId && $PokemonGlobal.pokecenterMapId>=0
+    pbCancelVehicles
+    pbRemoveDependencies
+    $game_temp.player_new_map_id    = $PokemonGlobal.pokecenterMapId
+    $game_temp.player_new_x         = $PokemonGlobal.pokecenterX
+    $game_temp.player_new_y         = $PokemonGlobal.pokecenterY
+    $scene.transfer_player if $scene.is_a?(Scene_Map)
+    $game_map.refresh
+  else#Home
+    $game_temp.player_new_map_id    = 9
+    $game_temp.player_new_x         = 16
+    $game_temp.player_new_y         = 23
+    $scene.transfer_player if $scene.is_a?(Scene_Map)
+    $game_map.refresh
+  end
+end
+
+
+def inCity?(city)
+  city_map_id = CITIES_MAP_IDS[city]
+  echoln "are we in city #{city}? #{city_map_id}, #{$PokemonGlobal.pokecenterMapId} "
+
+  return $PokemonGlobal.pokecenterMapId ==city_map_id
 end
