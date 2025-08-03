89350daa3a9270bf5447196bd7265a3951d1be56
Jdjohn307 <jdjohn307@gmail.com>
2025-08-03 09:54:48 -0700

    fix-level-cap: correct commit

--- 052_InfiniteFusion/New Items effects.rb
+++ 052_InfiniteFusion/New Items effects.rb
@@ -1658,23 +1658,28 @@ def pbUnfuse(pokemon, scene, supersplicers, pcPosition = nil)
       if pokemon.exp_when_fused_head == nil || pokemon.exp_when_fused_body == nil
         new_level = calculateUnfuseLevelOldMethod(pokemon, supersplicers)
         new_level = levelCap if ($PokemonSystem.level_caps==1 && new_level > levelCap)
+        poke1 = Pokemon.new(bodyPoke, new_level)
+        poke2 = Pokemon.new(headPoke, new_level)
       else
         new_level = pokemon.level
         exp_body = pokemon.exp_when_fused_body + pokemon.exp_gained_since_fused
         exp_head = pokemon.exp_when_fused_head + pokemon.exp_gained_since_fused
-
+        
         # Recalculate level and exp if pokemon would be over the level cap
         if ($PokemonSystem.level_caps==1)
           new_level = levelCap if new_level > levelCap
+          poke1 = Pokemon.new(bodyPoke, new_level)
+          poke2 = Pokemon.new(headPoke, new_level)
           exp_body = poke1.growth_rate.minimum_exp_for_level(levelCap) if poke1.growth_rate.minimum_exp_for_level(levelCap) < exp_body
           exp_head = poke2.growth_rate.minimum_exp_for_level(levelCap) if poke2.growth_rate.minimum_exp_for_level(levelCap) < exp_head
+        else
+          poke1 = Pokemon.new(bodyPoke, new_level)
+          poke2 = Pokemon.new(headPoke, new_level)
         end
-        
+
         poke1.exp = exp_body
         poke2.exp = exp_head
       end
-      poke1 = Pokemon.new(bodyPoke, new_level)
-      poke2 = Pokemon.new(headPoke, new_level)
       body_level = poke1.level
       head_level = poke2.level
 
