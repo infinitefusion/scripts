32db36854dea86fdedcbcf7d74eb1a07d8db9582
chardub <pkmninfinitefusion@gmail.com>
2025-10-02 10:33:43 -0400

    6.7.2 patch

--- 001_Settings.rb
+++ 001_Settings.rb
@@ -5,7 +5,7 @@
 #==============================================================================#
 module Settings
   # The version of your game. It has to adhere to the MAJOR.MINOR.PATCH format.
-  GAME_VERSION_NUMBER = "6.7.1"
+  GAME_VERSION_NUMBER = "6.7.2"
   LATEST_GAME_RELEASE = "6.6"
 
   KANTO = GAME_ID == :IF_KANTO
