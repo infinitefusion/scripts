aca4be853e735a2d137a4068cb038b40f8baf071
chardub <pkmninfinitefusion@gmail.com>
2026-01-02 12:04:09 -0500

    bday

--- 052_InfiniteFusion/Gameplay/Player/PlayerUtils.rb
+++ 052_InfiniteFusion/Gameplay/Player/PlayerUtils.rb
@@ -15,6 +15,12 @@ def isPostgame?()
   return $game_switches[SWITCH_BEAT_THE_LEAGUE]
 end
 
+def isPlayerBirthDate? #used only for the nurse to wish you happy birthday. The normal check needs to be deactivated before petalburg lol
+  return unless $Trainer.birth_day && $Trainer.birth_month
+  current_date = Time.now
+  return current_date.day == $Trainer.birth_day && current_date.month == $Trainer.birth_month
+end
+
 def isPlayerBirthDay?
   return false unless $game_switches[SWITCH_PETALBURG_WOODS_UNLOCKED]
   return unless $Trainer.birth_day && $Trainer.birth_month
