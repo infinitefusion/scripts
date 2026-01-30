45f9476ba915bd45d9d0fd1eb26a450d137eef66
chardub <pkmninfinitefusion@gmail.com>
2026-01-30 10:46:00 -0500

    Makes it possible to back out of hoenn starter selection menu + Fixes selected starter sprite not being applied

--- 053_PIF_Hoenn/StartersSelectionScene.rb
+++ 053_PIF_Hoenn/StartersSelectionScene.rb
@@ -1,7 +1,7 @@
 class StartersSelectionScene
-  POKEBALL_LEFT_X = -20;     POKEBALL_LEFT_Y = 70
-  POKEBALL_MIDDLE_X = 125;   POKEBALL_MIDDLE_Y = 100
-  POKEBALL_RIGHT_X = 275;    POKEBALL_RIGHT_Y = 70
+  POKEBALL_LEFT_X = -20; POKEBALL_LEFT_Y = 70
+  POKEBALL_MIDDLE_X = 125; POKEBALL_MIDDLE_Y = 100
+  POKEBALL_RIGHT_X = 275; POKEBALL_RIGHT_Y = 70
 
   TEXT_POSITION_X = 100
   TEXT_POSITION_Y = 10
@@ -10,30 +10,28 @@ class StartersSelectionScene
     @starters_species = starters
     @starter_pokemon = []
     @starters_species.each do |species|
-      @starter_pokemon.push(Pokemon.new(species,5))
+      @starter_pokemon.push(Pokemon.new(species, 5))
     end
 
     @spritesLoader = BattleSpriteLoader.new
-    @shown_starter_species=nil
+    @shown_starter_species = nil
   end
 
   def initializeGraphics()
     @background = displayPicture("Graphics/Pictures/Trades/hoenn_starter_bag_bg.png", -20, -20)
-    @background.z=0
+    @background.z = 0
 
     @foreground = displayPicture("Graphics/Pictures/Trades/hoenn_starter_bag_foreground.png", -20, -20)
-    @foreground.z=999
+    @foreground.z = 999
 
     @pokeball_closed_left = displayPicture("Graphics/Pictures/StarterSelection/trade_pokeball_closed_1.png", POKEBALL_LEFT_X, POKEBALL_LEFT_Y)
-    @pokeball_closed_left.z=2
+    @pokeball_closed_left.z = 2
 
     @pokeball_closed_middle = displayPicture("Graphics/Pictures/StarterSelection/trade_pokeball_closed_2.png", POKEBALL_MIDDLE_X, POKEBALL_MIDDLE_Y)
-    @pokeball_closed_middle.z=100
+    @pokeball_closed_middle.z = 100
 
     @pokeball_closed_right = displayPicture("Graphics/Pictures/StarterSelection/trade_pokeball_closed_3.png", POKEBALL_RIGHT_X, POKEBALL_RIGHT_Y)
-    @pokeball_closed_right.z=2
-
-
+    @pokeball_closed_right.z = 2
 
   end
 
@@ -53,31 +51,36 @@ class StartersSelectionScene
 
   def startScene
     initializeGraphics
-    @index=nil
+    @index = nil
     previous_index = nil
     loop do
       if @index
         if Input.trigger?(Input::RIGHT)
           previous_index = @index
-          @index+=1
+          @index += 1
           @index = 0 if @index == @starters_species.length
         end
         if Input.trigger?(Input::LEFT)
           previous_index = @index
-          @index-=1
-          @index = @starters_species.length-1 if @index < 0
+          @index -= 1
+          @index = @starters_species.length - 1 if @index < 0
         end
         if Input.trigger?(Input::UP) || Input.trigger?(Input::DOWN)
           updateOpenPokeballPosition
           updateStarterSelectionGraphics
         end
-
-          if Input.trigger?(Input::USE)
+        if Input.trigger?(Input::BACK)
+          pbPlayCancelSE
+          disposeGraphics
+          return nil
+        end
+        if Input.trigger?(Input::USE)
           if pbConfirmMessage(_INTL("Do you choose this Pokémon?"))
             chosenPokemon = @starter_pokemon[@index]
             @spritesLoader.registerSpriteSubstitution(@pif_sprite)
             disposeGraphics
-            pbSet(VAR_HOENN_CHOSEN_STARTER_INDEX,@index)
+            pbSet(VAR_HOENN_CHOSEN_STARTER_INDEX, @index)
+            chosenPokemon.pif_sprite = @pif_sprite
             return chosenPokemon
           end
         end
@@ -113,23 +116,22 @@ class StartersSelectionScene
   def updateClosedBallGraphicsVisibility
     case @index
     when 0
-      @pokeball_closed_left.visible=false
-      @pokeball_closed_middle.visible=true
-      @pokeball_closed_right.visible=true
+      @pokeball_closed_left.visible = false
+      @pokeball_closed_middle.visible = true
+      @pokeball_closed_right.visible = true
     when 1
-      @pokeball_closed_left.visible=true
-      @pokeball_closed_middle.visible=false
-      @pokeball_closed_right.visible=true
+      @pokeball_closed_left.visible = true
+      @pokeball_closed_middle.visible = false
+      @pokeball_closed_right.visible = true
     when 2
-      @pokeball_closed_left.visible=true
-      @pokeball_closed_middle.visible=true
-      @pokeball_closed_right.visible=false
+      @pokeball_closed_left.visible = true
+      @pokeball_closed_middle.visible = true
+      @pokeball_closed_right.visible = false
     else
-      @pokeball_closed_left.visible=true
-      @pokeball_closed_middle.visible=true
-      @pokeball_closed_right.visible=true
+      @pokeball_closed_left.visible = true
+      @pokeball_closed_middle.visible = true
+      @pokeball_closed_right.visible = true
     end
-
 
   end
 
@@ -145,18 +147,18 @@ class StartersSelectionScene
 
     case @index
     when 0
-      picture_back_path ="Graphics/Pictures/StarterSelection/BACKleftball"
+      picture_back_path = "Graphics/Pictures/StarterSelection/BACKleftball"
       picture_front_path = "Graphics/Pictures/StarterSelection/FRONTleftball"
     when 1
-      picture_back_path ="Graphics/Pictures/StarterSelection/BACKcenterball"
+      picture_back_path = "Graphics/Pictures/StarterSelection/BACKcenterball"
       picture_front_path = "Graphics/Pictures/StarterSelection/FRONTcenterball"
     when 2
-      picture_back_path ="Graphics/Pictures/StarterSelection/BACKrightball"
+      picture_back_path = "Graphics/Pictures/StarterSelection/BACKrightball"
       picture_front_path = "Graphics/Pictures/StarterSelection/FRONTrightball"
     end
 
-    @pokeball_open_back = displayPicture(picture_back_path,@shown_pokemon_x, @shown_pokemon_y,2)
-    @pokeball_open_front = displayPicture(picture_front_path,@shown_pokemon_x, @shown_pokemon_y,50)
+    @pokeball_open_back = displayPicture(picture_back_path, @shown_pokemon_x, @shown_pokemon_y, 2)
+    @pokeball_open_front = displayPicture(picture_front_path, @shown_pokemon_x, @shown_pokemon_y, 50)
 
     updatePokemonSprite
   end
@@ -176,7 +178,7 @@ class StartersSelectionScene
     @pokemonSpriteWindow.opacity = 0
     @pokemonSpriteWindow.z = 2
     @pokemonSpriteWindow.x = @shown_pokemon_x
-    @pokemonSpriteWindow.y = @shown_pokemon_y-10
+    @pokemonSpriteWindow.y = @shown_pokemon_y - 10
     updateText
   end
 
@@ -190,10 +192,10 @@ class StartersSelectionScene
     title_position_y = TEXT_POSITION_Y
     subtitle_position_y = TEXT_POSITION_Y + 30
 
-    text_x_offset=-100
+    text_x_offset = -100
 
-    label_base_color = Color.new(88,88,80)
-    label_shadow_color = Color.new(168,184,184)
+    label_base_color = Color.new(88, 88, 80)
+    label_shadow_color = Color.new(168, 184, 184)
 
     title_base_color = Color.new(248, 248, 248)
     title_shadow_color = Color.new(104, 104, 104)
@@ -207,10 +209,9 @@ class StartersSelectionScene
     @pokemon_category_overlay.font.size = 36
     @pokemon_category_overlay.font.name = MessageConfig.pbGetSmallFontName
 
-    pbDrawTextPositions(@pokemon_name_overlay, [[pokemon_name, (Graphics.width/2)+text_x_offset, title_position_y, 2, title_base_color, title_shadow_color]])
-    pbDrawTextPositions(@pokemon_category_overlay,[[pokemon_category, (Graphics.width/2)+text_x_offset, subtitle_position_y, 2, label_base_color, label_shadow_color]])
+    pbDrawTextPositions(@pokemon_name_overlay, [[pokemon_name, (Graphics.width / 2) + text_x_offset, title_position_y, 2, title_base_color, title_shadow_color]])
+    pbDrawTextPositions(@pokemon_category_overlay, [[pokemon_category, (Graphics.width / 2) + text_x_offset, subtitle_position_y, 2, label_base_color, label_shadow_color]])
 
   end
-
 
 end
