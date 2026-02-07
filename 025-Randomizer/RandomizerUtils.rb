8fd2e49997606a5029f8762040efdf381034888d
chardub <pkmninfinitefusion@gmail.com>
2026-02-07 12:22:02 -0500

    Fixes gym leader type messages

--- 025-Randomizer/RandomizerUtils.rb
+++ 025-Randomizer/RandomizerUtils.rb
@@ -157,7 +157,7 @@ def set_current_gym_type_name(gym_index = nil, capitalize=false, variable = VAR_
   end
   gym_types = get_gym_types_array
   current_type_id = gym_types[gym_index]
-  type_name = GameData::Type.get(current_type_id)
-  type_name = type_name.name.downcase unless capitalize
+  type_name = GameData::Type.get(current_type_id).real_name
+  type_name = type_name.downcase unless capitalize
   pbSet(variable, type_name)
 end
