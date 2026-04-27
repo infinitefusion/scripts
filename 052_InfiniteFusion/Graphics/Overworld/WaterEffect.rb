083f92984cb0a2a5f848c3021350a7f356b9685f
chardub <pkmninfinitefusion@gmail.com>
2026-04-27 12:56:15 -0400

    Replaces puddle animation by short version

--- 052_InfiniteFusion/Graphics/Overworld/WaterEffect.rb
+++ 052_InfiniteFusion/Graphics/Overworld/WaterEffect.rb
@@ -1,19 +1,22 @@
-PUDDLE_ANIMATION_ID = 22
+PUDDLE_ANIMATION_ID = 31
 Events.onStepTakenFieldMovement += proc { |_sender, e|
-  event = e[0] # Get the event affected by field movement
+  event = e[0]
   if $scene.is_a?(Scene_Map)
     event.each_occupied_tile do |x, y|
       mapTerrainTag = $MapFactory.getTerrainTag(event.map.map_id, x, y, false)
       if $PokemonGlobal.surfing
-        if isWaterTerrain?(mapTerrainTag) && !event.always_on_top #&& $PokemonGlobal.stepcount % 2 ==0
+        if isWaterTerrain?(mapTerrainTag) && !event.always_on_top
           $scene.spriteset.addUserAnimation(PUDDLE_ANIMATION_ID, event.x, event.y, true, 0)
+          break
         end
       elsif $PokemonGlobal.boat
         $scene.spriteset.addUserAnimation(PUDDLE_ANIMATION_ID, event.x, event.y-2, true, 0)
+        break
       else
-        if mapTerrainTag == 16 #puddle
-          pbSEPlay("puddle", 100) if event == $game_player && !$PokemonGlobal.surfing #only play sound effect in puddle
+        if mapTerrainTag == 16
+          pbSEPlay("puddle", 100) if event == $game_player && !$PokemonGlobal.surfing
           $scene.spriteset.addUserAnimation(PUDDLE_ANIMATION_ID, event.x, event.y, true, 0)
+          break
         end
       end
     end
