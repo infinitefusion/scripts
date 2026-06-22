86c93ed07de343f141e1fd048e655cb557a19f3a
Leon <152172548+Leonnovtaion@users.noreply.github.com>
2026-06-22 21:06:55 +0800

    Add _INTL

--- 020_Debug/003_Debug menus/005_Debug_PokemonCommands.rb
+++ 020_Debug/003_Debug menus/005_Debug_PokemonCommands.rb
@@ -736,7 +736,7 @@ PokemonDebugMenuCommands.register("setnature", {
     ids = []
     GameData::Nature.each do |nature|
       if nature.stat_changes.length == 0
-        commands.push("#{nature.real_name} (---)")
+        commands.push(_INTL("{1} (---)", nature.real_name))
       else
         plus_text = ""
         minus_text = ""
@@ -749,7 +749,7 @@ PokemonDebugMenuCommands.register("setnature", {
             minus_text += GameData::Stat.get(change[0]).name_brief
           end
         end
-        commands.push("#{nature.real_name} (+#{plus_text}, -#{minus_text})")
+        commands.push(_INTL("{1} (+{2}, -{3})", nature.real_name, plus_text, minus_text))
       end
       ids.push(nature.id)
     end
