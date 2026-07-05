2f6592811993594d98fe4f7a72f70891475b11a4
Leon <152172548+Leonnovtaion@users.noreply.github.com>
2026-07-05 14:01:40 +0800

    Update Chinese Translation Credits

--- 016_UI/001_Non-interactive UI/Credits/CREDITS_HOENN.rb
+++ 016_UI/001_Non-interactive UI/Credits/CREDITS_HOENN.rb
@@ -122,10 +122,11 @@ PokemonInfiniteFusionFR Discord
 Coordinated by anthonygourmand
 
 <title>Chinese Translation
-Leon (Leonnovation)
-白崎时绘 (Shiroe)
-阿省呦 (Sheng)
-小鱼转晴 (XploreLight)
+译者：Leon (Leonnovation)
+技术支持：白崎时绘 (Shiroe)
+文本参考：阿省呦 (Sheng)
+字体支持：狼人小林 (FusionPixel)
+其他支持：小鱼转晴 (XploreLight)
 
 The following free resources were also used 
 with their respective authors' consent:
