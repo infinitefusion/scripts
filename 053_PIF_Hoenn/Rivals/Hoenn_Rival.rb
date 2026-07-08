92982c70b10c04ee599d1dd0200fb8dd8fe92f0a
Leon <152172548+Leonnovtaion@users.noreply.github.com>
2026-07-08 13:20:35 +0800

    Fix: Fix the crash in the translated version during the first rival battle.

--- 053_PIF_Hoenn/Rivals/Hoenn_Rival.rb
+++ 053_PIF_Hoenn/Rivals/Hoenn_Rival.rb
@@ -306,7 +306,7 @@ end
 
 def initializeRivalBattledTrainer
   trainer_type = :RIVAL1
-  trainer_name = pbGet(VAR_RIVAL_NAME)
+  trainer_name = isPlayerMale ? "May" : "Brendan"
   trainer_appearance = $Trainer.rival_appearance
   rivalBattledTrainer = BattledTrainer.new(trainer_type, trainer_name, 0, BATTLED_TRAINER_RIVAL_KEY)
   rivalBattledTrainer.set_custom_appearance(trainer_appearance)
