78a8e6c4bd0575cb0672839db910a9c7d7cd53a0
Leon <152172548+Leonnovtaion@users.noreply.github.com>
2026-07-09 15:47:44 +0800

    Fix: Ensure that Glimmer Coins display the translated text correctly.

--- 050_Outfits/UI/clothesShop/ClothesShopView.rb
+++ 050_Outfits/UI/clothesShop/ClothesShopView.rb
@@ -2,7 +2,7 @@ class ClothesShopView < PokemonMart_Scene
 
   def initialize(currency_name = _INTL("Money"))
     @currency_name = currency_name
-    @currency_name = COSMETIC_CURRENCY_NAME if Settings::HOENN
+    @currency_name = cosmetic_currency_name if Settings::HOENN
   end
 
   def pbStartBuyOrSellScene(buying, stock, adapter)
